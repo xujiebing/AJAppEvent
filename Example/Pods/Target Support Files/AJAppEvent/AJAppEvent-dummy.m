3e028d63759fafe386f89970aeb1863678a7286e
xujiebing <xujiebing1992@gmail.com>
2019-11-12 07:22:40 +0800

    初始化

---
+++ Example/Pods/Target Support Files/AJAppEvent/AJAppEvent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AJAppEvent : NSObject
+@end
+@implementation PodsDummy_AJAppEvent
+@end
---
+++ Example/Pods/Target Support Files/Pods-AJAppEvent_Example/Pods-AJAppEvent_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AJAppEvent_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AJAppEvent_Example
+@end
